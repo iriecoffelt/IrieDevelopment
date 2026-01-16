c3b7014a725898d2f87924562cf34554bbd3e4c1
iriecoffelt <iriecoffelt@users.noreply.github.com>
2026-01-16 00:47:23 +0000

    deploy: 005f5bc4456f06b0fa5c7fead1c5bc1d24065e4f

---
+++ env-config.js
@@ -0,0 +1,9 @@
+// This file is generated during deployment
+// DO NOT commit secrets directly to the repository
+window.ENV_CONFIG = {
+  emailjsUserId: 'zRYVGu1o6DDmrdc4f',
+  emailjsServiceId: 'service_ju06a1p',
+  emailjsTemplateId: 'template_925ze9i',
+  jsonbinAccessKey: 'a0$.tte9gaAB5LnvRsH2dUz4OFuloHrp3GWDQsCdpRmgcfZ3uf4TYbsq',
+  jsonbinBinId: '6967037143b1c97be92f1730'
+};
