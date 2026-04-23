fa85d40e3eed6c4336987dff26a32d014c1bc757
iriecoffelt <iriecoffelt@users.noreply.github.com>
2026-04-23 23:57:08 +0000

    deploy: 096cda09c9d746a51394eda5db485201d2e977b3

--- env-config.js
+++ env-config.js
@@ -6,5 +6,5 @@ window.ENV_CONFIG = {
   emailjsUserId: 'zRYVGu1o6DDmrdc4f',
   emailjsServiceId: 'service_ju06a1p',
   emailjsTemplateId: 'template_925ze9i',
-  subscribersApiKey: ''
+  subscribersApiKey: '754f9c8cddbb11fcd3c08e52a8a97683c7c83ce9d691e3fc18be604316c20d99'
 };
